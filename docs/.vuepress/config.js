27130b42ff8ea3f766f58c4d9d66469dee1d5a9b
hhyang <1606726660@qq.com>
2019-11-26 21:40:49 +0800

    access Baidu Statistics

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -7,6 +7,17 @@ const {
 const path = require('path');
 module.exports = {
   title: 'uni-simple-router',
+  head: [
+    ['script', {}, `
+          var _hmt = _hmt || [];
+      (function() {
+        var hm = document.createElement("script");
+        hm.src = "https://hm.baidu.com/hm.js?dd407a6f6294639a47d73a050887a82b";
+        var s = document.getElementsByTagName("script")[0]; 
+        s.parentNode.insertBefore(hm, s);
+      })();
+    `]
+  ],
   description,
   themeConfig,
   markdown: {
